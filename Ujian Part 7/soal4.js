365c6b06f14d17f016ccc0be524b93154ac2fc5e
azharaufa <azharaufa221105@gmail.com>
2024-01-28 12:50:22 +0000

    Ujian After Part 7: Done!

---
+++ Ujian Part 7/soal4.js
@@ -0,0 +1,24 @@
+//   Problem
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string.
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
+function xo(str) {
+  let xJumlah = 0;
+  let oJumlah = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x") {
+      xJumlah++;
+    } else {
+      oJumlah++;
+    }
+  }
+  return xJumlah === oJumlah;
+}
+
+// TEST CASES
+console.log(xo("xoxoxo")); // true
+console.log(xo("oxooxo")); // false
+console.log(xo("oxo")); // false
+console.log(xo("xxxooo")); // true
+console.log(xo("xoxooxxo")); // true
