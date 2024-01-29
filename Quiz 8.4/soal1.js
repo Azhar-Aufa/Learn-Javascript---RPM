06a7cc210f9116dbb2e5048176daa1ad19beb830
azharaufa <azharaufa221105@gmail.com>
2024-01-29 07:38:11 +0000

    Quiz 8.4, soal1: Done!

--- Quiz 8.4/soal1.js
+++ Quiz 8.4/soal1.js
@@ -2,10 +2,11 @@
 Diberikan sebuah function palindrome(kata) yang menerima satu parameter. Function tersebut akan mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan. Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'.
 */
 
-function palindrome(kata) {
-    let kata = ''
-    
-}
+// function palindrome(kata) {
+//   return kata == kata.split("").reverse().join("");
+// }
+
+let palindrome = (kata) => kata == kata.split("").reverse().join("");
 
 // TEST CASES
 console.log(palindrome("katak")); // true
@@ -13,3 +14,6 @@ console.log(palindrome("blanket")); // false
 console.log(palindrome("civic")); // true
 console.log(palindrome("kasur rusak")); // true
 console.log(palindrome("mister")); // false
+console.log(palindrome("bagab"));
+console.log(palindrome("susus"));
+console.log(palindrome("bitcoin"));
